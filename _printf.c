786ac030c217cfaaeefc3bc5a926ea95cbb7ad9f
Toliva <tisopar@gmail.com>
2022-05-17 13:00:31 -0700

    handle conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _printf - Prpduce output according to our format
+ * _printf - Produces output according to a format.
  *
- * @format:
- * @param ...:
- * @return int
+ * @format: The format to be printed
+ * @...: The arguments to be used by the funtion
+ * Return: 0
  */
 int _printf(const char *format, ...)
 {
@@ -23,6 +23,25 @@ int _printf(const char *format, ...)
 		else if (format[i] == '%')
 		{
 			if (format[i + 1] == 'c')
+			{
+				_putc(va_arg(args, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				_prints(va_arg(args, char *));
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				_putc('%');
+				i++;
+			}
+			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				_printi(va_arg(args, int));
+				i++;
+			}
 		}
 		i++;
 	}
