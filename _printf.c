2c778d9ecde8238cb7f556549ae8637679b837f0
Toliva <tisopar@gmail.com>
2022-05-19 00:59:03 -0700

    update function

--- _printf.c
+++ _printf.c
@@ -2,7 +2,6 @@
 
 /**
  * _printf - Produces output according to a format.
- *
  * @format: The format to be printed
  * @...: The arguments to be used by the funtion
  * Return: 0
@@ -10,41 +9,41 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0;
+	int i = 0, count = 0;
 
 	va_start(args, format);
-
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
 		{
 			_putc(format[i]);
+			count++;
 		}
 		else if (format[i] == '%')
 		{
 			if (format[i + 1] == 'c')
 			{
 				_putc(va_arg(args, int));
-				i++;
+				count++, i++;
 			}
 			else if (format[i + 1] == 's')
 			{
 				_prints(va_arg(args, char *));
-				i++;
+				count++, i++;
 			}
 			else if (format[i + 1] == '%')
 			{
 				_putc('%');
-				i++;
+				count++, i++;
 			}
 			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
 			{
 				_printi(va_arg(args, int));
-				i++;
+				count++, i++;
 			}
 		}
 		i++;
 	}
 	va_end(args);
-	return (i);
+	return (count);
 }
