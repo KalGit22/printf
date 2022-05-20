c41bbdfc715687917b4bef861ab99cc625de5b2e
Kalkidan Simegn Adimasu <kalkidan>
2022-05-20 14:34:16 -0700

    update

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * _printf - Produces output according to a format.
  * @format: The format to be printed
  * @...: The arguments to be used by the funtion
- * Return: count of charactes printed
+ * Return: count of characte
  */
 int _printf(const char *format, ...)
 {
@@ -15,29 +15,33 @@ int _printf(const char *format, ...)
 		int i = 0;
 
 		va_list args;
-		int  (*func)(va_list);
+		int (*func)(va_list);
 
 		va_start(args, format);
 		if (format[0] == '%' && format[1] == '\0')
 			return (count);
 		count = 0;
-		if (format[i] != '%')
+		while (format[i] != '\0')
 		{
-			count += _putc(format[i]);
-		}
-		else
-		{
-			if (format[i + 1] == '%')
+			if (format[i] != '%')
 			{
-				count += _putc('%');
-				i++;
+				count += _putc(format[i]);
 			}
 			else
 			{
-				func = _select(format[i + 1]);
-				count += (func ? func(args) : _putc(format[i]) + _putc(format[i + 1]));
-				i++;
+				if (format[i + 1] == '%')
+				{
+					count += _putc('%');
+					i++;
+				}
+				else
+				{
+					func = _select(format[i + 1]);
+					count += (func ? func(args) : _putc(format[i]) + _putc(format[i + 1]));
+					i++;
+				}
 			}
+			i++;
 		}
 		va_end(args);
 	}
